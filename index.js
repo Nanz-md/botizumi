a937926ee56461ced48c5bf5b1d1de55715dcc8c
Nanz-md <108403508+Nanz-md@users.noreply.github.com>
2022-11-10 17:07:45 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,161 +1,101 @@
-/**
- - Created By Lexxy Official
- - Base Neobot Multi Device
- - Silahkan Di Pakai & Kembangkan Fiturnya
-*/
+const mineflayer = require('mineflayer')
+const cmd = require('mineflayer-cmd').plugin
+const fs = require('fs');
+let rawdata = fs.readFileSync('config.json');
+let data = JSON.parse(rawdata);
+var lasttime = -1;
+var moving = 0;
+var first = false;
+var connected = 0;
+var actions = [ 'forward', 'back', 'left', 'right']
+var lastaction;
+var pi = 3.14159;
+var moveinterval = 2; // 2 second movement interval
+var maxrandom = 3; // 0-5 seconds added to movement interval (randomly)
+var host = data["ip"];
+var username = data["name"]
+var nightskip = data["auto-night-skip"]
+var bot = mineflayer.createBot({
+  host: host,
+  port:data["port"],
+  username: username
+});
+function getRandomArbitrary(min, max) {
+       return Math.random() * (max - min) + min;
 
-"use strict";
-const {
-	default: makeWASocket,
-	BufferJSON,
-	initInMemoryKeyStore,
-	DisconnectReason,
-	AnyMessageContent,
-        makeInMemoryStore,
-	useSingleFileAuthState,
-	delay
-} = require("@adiwajshing/baileys")
-const figlet = require("figlet");
-const fs = require("fs");
-const moment = require('moment')
-const chalk = require('chalk')
-const logg = require('pino')
-const clui = require('clui')
-const { Spinner } = clui
-const { serialize, getBuffer } = require("./lib/myfunc");
-const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require("./lib/exifff");
-const { color, mylog, infolog } = require("./lib/color");
-const time = moment(new Date()).format('HH:mm:ss DD/MM/YYYY')
-let setting = JSON.parse(fs.readFileSync('./config.json'));
-let session = `./${setting.sessionName}.json`
-const { state, saveState } = useSingleFileAuthState(session)
-let welcome = JSON.parse(fs.readFileSync('./database/welcome.json'));
-
-function title() {
-	  console.log(chalk.bold.blue(figlet.textSync('NEOBOT', {
-		font: 'Standard',
-		horizontalLayout: 'default',
-		verticalLayout: 'default',
-		width: 80,
-		whitespaceBreak: false
-	})))
-	console.log(chalk.yellow(`\n ${chalk.green('[ Created By Nanz-MD ]')}\n`))
-}
-/**
-* Uncache if there is file change;
-* @param {string} module Module name or path;
-* @param {function} cb <optional> ;
-*/
-function nocache(module, cb = () => { }) {
-fs.watchFile(require.resolve(module), async () => {
-await uncache(require.resolve(module))
-cb(module)
-	})
-}
-/**
-* Uncache a module
-* @param {string} module Module name or path;
-*/
-function uncache(module = '.') {
-	return new Promise((resolve, reject) => {
-		try {
-			delete require.cache[require.resolve(module)]
-			resolve()
-		} catch (e) {
-			reject(e)
-		}
-	})
 }
 
-const status = new Spinner(chalk.cyan(` Booting WhatsApp Bot`))
-const starting = new Spinner(chalk.cyan(` Preparing After Connect`))
-const reconnect = new Spinner(chalk.redBright(` Reconnecting WhatsApp Bot`))
+bot.loadPlugin(cmd)
 
-const store = makeInMemoryStore({ logger: logg().child({ level: 'fatal', stream: 'store' }) })
 
-const connectToWhatsApp = async () => {
-	const conn = makeWASocket({
-            printQRInTerminal: true,
-            logger: logg({ level: 'fatal' }),
-            auth: state,
-            browser: ["Neobot Multi Device", "Safari", "3.0"]
-        })
-	title()
-        store.bind(conn.ev)
+
+bot.on('login',function(){
+	console.log("Mencoba Login")
+	if(data["login-enabled"] == "true"){
+		bot.chat(data["register-cmd"])
+		bot.chat(data["login-cmd"])
+	}
+	for (let i=0; i<10; i++) { 
+	   task(i); 
+	} 
+	console.log("Sukses Login")
+	bot.chat("Halo semua");
+});
+  
+function task(i) { 
 	
-	/* Auto Update */
-	require('./message/help')
-	require('./message/msg')
-	nocache('./message/help', module => console.log(chalk.yellow(`'./message/help.js' Telah diupdate`)))
-	nocache('./message/msg', module => console.log(chalk.yellow(`'./message/msg.js' Telah diupdate`)))
-	
-	conn.multi = true
-	conn.nopref = false
+  setTimeout(function() { 
+    if(first == true){
+	bot.chat("Selamat bergabung di server ini")
+	first = false;
+	}
+	else{
+		bot.chat("Diperbarui oleh vanevgt")
+		first = true;
+	}
+  }, 3600000 * i); 
+} 
 
-	conn.prefa = 'anjing'
-	conn.ev.on('messages.upsert', async m => {
-		if (!m.messages) return;
-		var msg = m.messages[0]
-		msg = serialize(conn, msg)
-		msg.isBaileys = msg.key.id.startsWith('BAE5') || msg.key.id.startsWith('3EB0')
-		require('./message/msg')(conn, msg, m, setting, store, welcome)
-	})
-	conn.ev.on('connection.update', (update) => {
-		const { connection, lastDisconnect } = update
-		if (connection === 'close') {
-			status.stop()
-			reconnect.stop()
-			starting.stop()
-			console.log(mylog('Server Ready ✓'))
-			lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut 
-			? connectToWhatsApp()
-			: console.log(mylog('Wa web terlogout...'))
-		}
-	})
-	conn.ev.on('creds.update', () => saveState)
-	
-        conn.ev.on('group-participants.update', async (data) => {
-          const isWelcome = welcome.includes(data.id) ? true : false
-          if (isWelcome) {
-            try {
-            	let metadata = await conn.groupMetadata(data.id)
-              for (let i of data.participants) {
-                try {
-                  var pp_user = await conn.profilePictureUrl(i, 'image')
-                } catch {
-                  var pp_user = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
-                }
-                if (data.action == "add") {
-                  var but = [{buttonId: `/`, buttonText: { displayText: "Welcome" }, type: 1 }, {buttonId: `/infobot`, buttonText: { displayText: "Siapa si aku?" }, type: 1 }]
-				conn.sendMessage(data.id, { caption: `Hallo @${i.split("@")[0]} Selamat Datang Di Grup *${metadata.subject}*\n\nIntro Dulu Yuk Kak\n\n\nNama : \nUmur :\nAskot :\nGender :\n\nSemoga Kamu Betah Di Grup ini, Jangan Lupa Untuk Membaca Dan Mematuhi Rules Yang Ada`, image: {url: pp_user}, buttons: but, footer: `Deskripsi : ${metadata.desc}`, mentions: [i]})
-		        } else if (data.action == "remove") {
-		          var but = [{buttonId: `/`, buttonText: { displayText: "Bye" }, type: 1 }]
-				conn.sendMessage(data.id, { caption: `Selamat Tinggal @${i.split("@")[0]}\n\nSemoga Harimu Suram\nTetap Putus Asa Jangan Semangat Dan Jadilah Beban Keluarga 🤙🗿`, image: {url: pp_user}, buttons: but, footer: `${metadata.subject}`, mentions: [i]})
-		}
-              }
-            } catch (e) {
-              console.log(e)
-            }
-          }
-        })
-        
-     conn.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
-        let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
-        let buffer
-        if (options && (options.packname || options.author)) {
-            buffer = await writeExifImg(buff, options)
-        } else {
-            buffer = await imageToWebp(buff)
-        }
 
-        await conn.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
-        return buffer
+bot.on('time', function(time) {
+
+		
+	if(nightskip == "true"){
+	if(bot.time.timeOfDay >= 13000){
+	bot.chat('/time set day')
+	}}
+    if (connected <1) {
+        return;
     }
-    
-	conn.reply = (from, content, msg) => conn.sendMessage(from, { text: content }, { quoted: msg })
-    conn.sendText = (from, text, quoted = '', options) => conn.sendMessage(from, { text: text, ...options }, { quoted })
-	return conn
-}
+    if (lasttime<0) {
+        lasttime = bot.time.age;
+    } else {
+        var randomadd = Math.random() * maxrandom * 20;
+        var interval = moveinterval*20 + randomadd;
+        if (bot.time.age - lasttime > interval) {
+            if (moving == 1) {
+                bot.setControlState(lastaction,false);
+                moving = 0;
+                lasttime = bot.time.age;
+            } else {
+                var yaw = Math.random()*pi - (0.5*pi);
+                var pitch = Math.random()*pi - (0.5*pi);
+                bot.look(yaw,pitch,false);
+                lastaction = actions[Math.floor(Math.random() * actions.length)];
+                bot.setControlState(lastaction,true);
+                moving = 1;
+                lasttime = bot.time.age;
+                bot.activateItem();
+            }
+        }
+    }
+});
 
-connectToWhatsApp()
-.catch(err => console.log(err))
+bot.on('spawn',function() {
+    connected=1;
+});
+
+bot.on('death',function() {
+    bot.emit("respawn")
+});
+
